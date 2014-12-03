994dfd5381e94ce8d88f7da2386bc545e65da304
thisIsSparta <yafeunteun@MacBook-Air-de-Feunteun.local>
2014-12-03 20:59:22 +0100

    Bubble sort test added

--- BACKEND/backEnd/Probe.cs
+++ BACKEND/backEnd/Probe.cs
@@ -39,6 +39,11 @@ namespace backEnd
 		public void incrNbComparisons() {
 			++nbComparisons;
 		}
+
+		public override string ToString ()
+		{
+			return string.Format ("[Probe: nbComparisons={0}, nbCopies={1}, nbExchanges={2}]", nbComparisons, nbCopies, nbExchanges);
+		}
 	}
 }
 
