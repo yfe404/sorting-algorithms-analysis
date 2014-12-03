23477e5e9bb0052c963a6d1ee65085410a8801c1
Maxime Mouchet <max@maxmouchet.com>
2014-12-03 15:32:08 +0100

    Add base C# App

---
+++ SIT152_TP2/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SIT152_TP2
+{
+	public class Application
+	{
+		public static void Main ()
+		{
+			Gtk.Application.Init ();
+			new Application ();
+			Gtk.Application.Run ();
+		}
+
+		public Application ()
+		{
+			Gtk.Window window = new Gtk.Window ("Comparaison algorithmes");
+			window.SetSizeRequest (500,200);
+
+			Gtk.VPaned vpane = new Gtk.VPaned ();
+			window.Add (vpane);
+
+			window.ShowAll ();
+		}
+	}
+}
+
